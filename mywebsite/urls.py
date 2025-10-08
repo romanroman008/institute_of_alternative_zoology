5cdf23cfc4777f0ab3ee5ebb05d8136701a51951
Mattew <me@example.com>
2025-10-08 14:55:22 +0200

    fix: urls

--- mywebsite/urls.py
+++ mywebsite/urls.py
@@ -18,11 +18,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-
-
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", RedirectView.as_view(url=reverse_lazy("myapp:index"), permanent=False)),
     path('myapp/', include('myapp.urls')),
     path('users/', include('users.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
